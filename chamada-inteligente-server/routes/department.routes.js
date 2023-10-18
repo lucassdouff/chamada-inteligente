6d8c08708d03e522c34171ad3f19a8b996648aef
LeandroFariasMatos <leandromatos95@gmail.com>
2023-10-17 21:16:34 -0300

    #{18} - Added attendance

--- chamada-inteligente-server/routes/department.routes.js
+++ chamada-inteligente-server/routes/department.routes.js
@@ -1,8 +1,13 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
 const departmentController = require('../controller/department');
 
-router.delete('/department/:id', departmentController.deleteDepartment);
+router.get('/getDepartments', departmentController.getAllDepartment);
+router.get('/ByName', departmentController.getDepartmentByName);
+router.get('/ById', departmentController.getDepartmentById);
+router.post('/addDepartment', departmentController.createDepartment);
+router.put('/updateDepartment/:id', departmentController.updateDepartment);
+router.delete('/deleteDepartmen/t:id', departmentController.deleteDepartment);
 
 module.exports = router;
