932201090b71ec525b398e70edcf316daef693d8
Henrique Porto <henriqueporto@id.uff.br>
2023-10-19 19:57:07 -0300

    #47 - getStudentAttendanceStats

--- chamada-inteligente-server/routes/attendance.routes.js
+++ chamada-inteligente-server/routes/attendance.routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const attendanceController = require('../controller/attendance');
 
 router.post('/addAttendance', attendanceController.createAttendance);
-
+router.get('/stats/:id_class/:id_student', attendanceController.getStudentAttendanceStats);
 
 module.exports = router;
