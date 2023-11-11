9cb3ffb3ea575c8855945461d2fe426a489c3e63
Lucas <lucas.souza0673@gmail.com>
2023-11-11 16:52:14 -0300

    #97 - integrando logout de usuario

--- src/app/navigation/teacher/TeacherDrawerNavigation.tsx
+++ src/app/navigation/teacher/TeacherDrawerNavigation.tsx
@@ -1,38 +1,43 @@
 import { DrawerContentScrollView, DrawerItem, DrawerItemList, createDrawerNavigator } from "@react-navigation/drawer";
 import { HomeNavigation } from "./Home/HomeNavigation";
 import { NotificationsNavigation } from "./Notifications/NotificatiosNavigation";
-import { CommonActions, useNavigation } from "@react-navigation/core";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { navigationController } from "../../../core/controllers/NavigationController";
+import { useNavigation } from "@react-navigation/core";
 import React from "react";
 import { View } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import LoginScreen from "../../screens/login/Login";
+import { navigationController } from "../../../core/controllers/NavigationController";
 
 const Drawer = createDrawerNavigator();
 
 function AppDrawerContent (props:any) {
 
-    const navigation = useNavigation();
+    const navigation = useNavigation<StackNavigationProp<any>>();
+    const { setSession } = navigationController();
 
     return (
        <DrawerContentScrollView {...props} contentContainerStyle={{flex:1}}>
          <DrawerItemList {...props} />
          <View style={{flex:1,marginVertical:20}}>
-           {/* here's where you put your logout drawer item*/}
            <DrawerItem 
              label="Log out"
-             onPress={()=>{}
-             }
+             onPress={()=>{
+                setSession(undefined);
+                navigation.navigate('Logout');
+             }}
              style={{flex:1,justifyContent:'flex-end'}}
            />
          </View>
        </DrawerContentScrollView>
      );
-   }
-
+}
 
 export default (props: any) => (
     <Drawer.Navigator drawerContent={props=><AppDrawerContent {...props}/>} screenOptions={{ headerShown: false }}>
         <Drawer.Screen name="Turmas" component={HomeNavigation} />
         <Drawer.Screen name="Notificações" component={NotificationsNavigation} />
+        <Drawer.Screen options={{
+          drawerItemStyle: { display: 'none' }
+        }} name="Logout" component={LoginScreen} />
     </Drawer.Navigator>
 );
